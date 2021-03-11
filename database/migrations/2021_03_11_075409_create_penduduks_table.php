f97431d7214bd8a8134a2e507df6165ade368322
Bila Arta <bilaarta@gmail.com>
2021-03-11 15:23:17 +0700

    create table penduduk

--- database/migrations/2021_03_11_075409_create_penduduks_table.php
+++ database/migrations/2021_03_11_075409_create_penduduks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSuratsTable extends Migration
+class CreatePenduduksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateSuratsTable extends Migration
      */
     public function up()
     {
-        Schema::create('surats', function (Blueprint $table) {
+        Schema::create('penduduks', function (Blueprint $table) {
             $table->id();
-            $table->string('subjek');
-            $table->string('deskripsi');
-            $table->foreignId('pendudukId')->constrained('penduduks');
+            $table->string('nama');
+            $table->string('nik')->unique();
+            $table->date('tanggalLahir');
+            $table->string('gender');
+            $table->string('alamat');
             $table->timestamps();
         });
     }
@@ -29,6 +31,6 @@ class CreateSuratsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('surats');
+        Schema::dropIfExists('penduduks');
     }
 }
--- database/migrations/2021_03_11_075543_create_surat_penduduks_table.php
+++ database/migrations/2021_03_11_075543_create_surat_penduduks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSuratsTable extends Migration
+class CreateSuratPenduduksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateSuratsTable extends Migration
      */
     public function up()
     {
-        Schema::create('surats', function (Blueprint $table) {
-            $table->id();
-            $table->string('subjek');
-            $table->string('deskripsi');
-            $table->foreignId('pendudukId')->constrained('penduduks');
-            $table->timestamps();
+        Schema::create('surat_penduduks', function (Blueprint $table) {
+            $table->foreignId('suratId')->constrained('surats')->onDelete('cascade');
+            $table->foreignId('pendudukId')->constrained('penduduks')->onDelete('cascade');
         });
     }
 
@@ -29,6 +26,6 @@ class CreateSuratsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('surats');
+        Schema::dropIfExists('surat_penduduks');
     }
 }
